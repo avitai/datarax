942009935c6d1999a698211ebcadc5360ccffb7a
Mahdi Shafiei <m.mahdi.shafiei@gmail.com>
2026-09-21 10:50:18 -0700

    chore(release): datarax 0.1.15 (#33)

--- src/datarax/__init__.py
+++ src/datarax/__init__.py
@@ -39,7 +39,7 @@ from datarax.typing import Batch, Element
 from datarax.utils.multirate import multirate_align
 
 
-__version__ = "0.1.14"
+__version__ = "0.1.15"
 
 __all__ = [
     # Type aliases
